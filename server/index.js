2feabedb9e577fb8ffafb7397984e4f3d2564d44
jackle <jridylan@gmail.com>
2024-01-05 18:37:33 +0800

    added api

--- server/index.js
+++ server/index.js
@@ -1,17 +1,60 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const Product = require('./productSchema');
+const Menu = require('./model/menuSchema');
 require('dotenv').config();
 const app = express();
+const cors = require('cors');
+
 
 // Middleware
 app.use(express.json());
-// Port Configuration
-const port = process.env.PORT || 3000;
+app.use(cors());
 
-mongoose.connect(process.env.DB)
+// Port Configuration
+const port = process.env.PORT;
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB, { dbName: 'cafe' })
     .then(() => {
-        console.log('Connected to Atlas!')
-        app.listen(port)
-        console.log(`Server listens on PORT ${port}`)})
-    .catch((err) => console.log(err))
+        console.log('Successfully connected to MongoDB Atlas')
+    })
+    .catch((err) => {
+        console.log('Failed to connect to MongoDB Atlas', err)
+        process.exit(1)
+    })
+
+
+// Define Routes
+app.get('/api/get-menu', async (req, res) => {
+    try {
+        const items = await Menu.find({});
+        res.status(200).send(items);
+        console.log(items);
+    } catch (error) {
+        console.log({ message: error });
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+})
+
+app.get('/api/get-menu/:category', async (req, res) => {
+    try {
+        const filteredMenu = req.params.category;
+        const items = await Menu.find({ category: filteredMenu}).exec();
+        res.status(200).send(items)
+        console.log(items)
+    } catch (error) {
+        console.log({ message: error });
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+})
+
+// Start Express Server
+app.listen(port, () => {
+    console.log(`Server listens on PORT ${port}`)
+})
+
+process.on('SIGINT', async () => {
+    await mongoose.connection.close()
+    console.log('MongoDB connection closed through app termination')
+    process.exit(0)
+});
--- vite-app/src/pages/Menu.jsx
+++ vite-app/src/pages/Menu.jsx
@@ -1,8 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import  ProductList  from "../components/ProductCard/ProductList";
 import { Cart } from "../components/Cart/Cart";
-import { MENU } from "../database/menu";
+import { VscCoffee } from "react-icons/vsc";
+import { GiTacos } from "react-icons/gi";
+import { IoFastFoodOutline } from "react-icons/io5";
 
 function Menu() {
+  const [Menu, setMenu] = useState([]);
+  const [filter, setFilter] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = filter
+          ? await axios.get(`/api/get-menu/${filter}`)
+          : await axios.get('/api/get-menu');
+
+          setMenu(response.data);
+          console.log(response.data);
+      } catch (error) {
+        console.error(error)
+      }
+    } 
+    fetchData();
+  }, [filter]);
+
+  const handleFilter = (id) => {
+    setFilter(id);
+  };
+
   return (
     <div className="menu bg-[#f5f5ef]">
       <div className="w-full bg-[#f5f5ef] flex">
@@ -11,8 +38,15 @@ function Menu() {
         <div className="basis-1/4">
           <Cart />
         </div>
-        <div className="basis-3/4">
-          <ProductList products={MENU} />
+        <div className="basis-3/4 flex">
+          <ProductList products={Menu} />
+        </div>
+        <div className="relative text-4xl">
+            <div className="bg-white shadow-lg fixed flex flex-col border-1 rounded-lg top-28 right-6">
+              <button className="p-2 hover:bg-slate-200" onClick={() => handleFilter(null)}><IoFastFoodOutline /></button>
+              <button className="p-2 hover:bg-slate-200" onClick={() => handleFilter("coffee")}><VscCoffee /></button>
+              <button className="p-2 hover:bg-slate-200" onClick={() => handleFilter("meals")}><GiTacos /></button>
+            </div>
         </div>
       </div>
     </div>
