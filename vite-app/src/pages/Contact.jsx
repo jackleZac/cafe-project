ede9a1670fd97d1cfefb4fbb634574f4c3e36e39
jackle <jridylan@gmail.com>
2024-02-13 17:46:52 +0800

    updated

--- vite-app/src/pages/Contact.jsx
+++ vite-app/src/pages/Contact.jsx
@@ -4,48 +4,53 @@ import cafeDesign5  from '../assets/cafeDesign5.jpg';
 function Contact() {
   return (
     <div className='grid grid-cols-2 max-lg:grid-cols-1 bg-[#f5f5ef]'>
-      <div className='relative h-screen pt-24' >
+      <div className='relative h-screen' >
         <img src={cafeDesign5} className='absolute z-10 w-full h-full'/>
-        <form className='absolute px-6 py-12 z-20 w-2/3 h-2/3 mx-auto text-base text-white bg-[#545454] opacity-80'>
-          <div className='relative flex flex-row max-xl:flex-col my-2'>
-            <div className='mr-2 '>
-              <label htmlFor="fname">First Name</label><br />
+        <div className='absolute top-36 z-20 w-full'>
+        <form className='relative px-6 py-6 w-fit mx-auto text-base text-white bg-[#545454] opacity-90'>
+          <div className='flex flex-row  max-sm:flex-col my-2 sm:space-x-2'>
+            <div className=''>
+              <label htmlFor="fname">FIRST NAME</label><br />
               <input type="text" id='fname' className='px-2 text-black'/>
             </div>
-            <div className='ml-2 '>
-              <label htmlFor="lname">Last Name</label><br />
-              <input type="text" id='lname' className=' px-2 text-black'/>
+            <div className=''>
+              <label htmlFor="lname">LAST NAME</label><br />
+              <input type="text" id='lname' className='px-2 text-black'/>
+            </div>
+          </div>
+          <div className='flex flex-row max-sm:flex-col my-2 sm:space-x-2'>
+            <div className=''>
+              <label htmlFor="phone">PHONE</label><br />
+              <input type="text" id='phone' className='px-2 text-black' />
+            </div>
+            <div className=''>
+              <label htmlFor="email">EMAIL</label><br />
+              <input type="email" id='email' className='px-2 text-black'/>
             </div>
           </div>
           <div>
-            <label htmlFor="phone">Phone</label><br />
-            <input type="number" id='phone' className='w-2/3 px-2 text-black' />
+            <label htmlFor="inquiry">INQUIRY</label><br />
+            <textarea id='inquiry' className='w-full px-2 text-black'></textarea>
           </div>
-          <div>
-            <label htmlFor="email">Email</label><br />
-            <input type="email" id='email' className='w-2/3 px-2 text-black'/>
-          </div>
-          <div>
-            <label htmlFor="inquiry">Inquiry</label><br />
-            <textarea id='inquiry' className='w-2/3 px-2 text-black'></textarea>
-          </div>
-          <button className='p-2 my-6 bg-[#373333] opacity-none'>SUBMIT</button>
+          <button className='p-2 my-6 bg-[#373333]'>SUBMIT</button>
+          <p>Leave Your Message Here <br />We'll Get In Touch With You Shortly</p>
         </form>
+        </div>
       </div>
       <div className='text-black'>
-        <div className='mx-auto'>
+        <div className='mx-auto w-fit flex flex-col space-y-6'>
           <h1 className='mt-48 text-2xl font-bold'>SEE YOU SOON</h1>
           <div>
             <h2>PHONE NUMBER</h2>
-            <p>(123) 456-7890</p>
+            <p>(60) 18KLP00YL</p>
           </div>
           <div>
             <h2>EMAIL ADDRESS</h2>
-            <p>hello@reallygreatsite.com</p>
+            <p>hello@artisanbrews.com</p>
           </div>
           <div>
             <h2>LOCATION</h2>
-            <p>23 Anywhere St. Any City, ST 12345</p>
+            <p>Gala City Commercial Centre, <br />Jalan Datuk Tawi Sli 3rd Exchange <br />Commercial Centre, 93250 Kuching, Sarawak</p>
           </div>
         </div>
       </div>
